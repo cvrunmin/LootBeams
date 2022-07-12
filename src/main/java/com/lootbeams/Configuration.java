a77f6863ed981dfb6ce93add8971f0eb2167494a
cvrunmin <cvrunmin@outlook.com>
2022-07-12 21:17:17 +0800

    change config format back to toml
    NOFIX: indentation issue of array is the issue of toml4j

--- src/main/java/com/lootbeams/Configuration.java
+++ src/main/java/com/lootbeams/Configuration.java
@@ -3,7 +3,8 @@ package com.lootbeams;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonWriter;
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
 import com.mojang.datafixers.util.Pair;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.HolderSet;
@@ -13,10 +14,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -58,28 +58,88 @@ public class Configuration {
 	public boolean whiteRarities = false;
 
 	public static Configuration load(){
-		File configFile = FabricLoader.getInstance().getConfigDir().resolve(LootBeams.MODID + ".json").toFile();
+		File configFile = FabricLoader.getInstance().getConfigDir().resolve(LootBeams.MODID + "-client.toml").toFile();
+		Configuration inst = new Configuration();
 		if (configFile.isFile()) {
-			try {
-				Configuration fromJson = GSON.fromJson(new FileReader(configFile), Configuration.class);
-				if (fromJson == null) fromJson = new Configuration();
-				return fromJson;
-			} catch (IOException e) {
-				LootBeams.LOGGER.warn("cannot load config file", e);
-			}
+			var main = new Toml().read(configFile).getTable("Loot Beams");
+			inst.renderNameColor = main.getBoolean("render_name_color", true);
+			inst.renderRarityColor = main.getBoolean("render_rarity_color", true);
+			inst.beamRadius = main.getDouble("beam_radius", 1.0).floatValue();
+			inst.beamHeight = main.getDouble("beam_height", 1.0).floatValue();
+			inst.beamYOffset = main.getDouble("beam_y_offset", 0.0).floatValue();
+			inst.beamAlpha = main.getDouble("beam_alpha", 0.85).floatValue();
+			inst.renderDistance = main.getDouble("render_distance", 24.0).floatValue();
+			inst.colorOverrides = main.getList("color_overrides", new ArrayList<>());
+
+			var items = main.getTable("Items");
+			inst.allItems = items.getBoolean("all_items", true);
+			inst.onlyRare = items.getBoolean("only_rare", false);
+			inst.onlyEquipment = items.getBoolean("only_equipment", false);
+			inst.whitelist = items.getList("whitelist", new ArrayList<>());
+			inst.blacklist = items.getList("blacklist", new ArrayList<>());
+
+			var nametags = main.getTable("Nametags");
+			inst.borders = nametags.getBoolean("borders", true);
+			inst.renderNametags = nametags.getBoolean("render_nametags", true);
+			inst.renderNametagsOnlook = nametags.getBoolean("render_nametags_onlook", true);
+			inst.renderStackcount = nametags.getBoolean("render_stackcount", true);
+			inst.nametagLookSensitivity = nametags.getDouble("nametag_look_sensitivity", 0.018).floatValue();
+			inst.nametagTextAlpha = nametags.getDouble("nametag_text_alpha", 1.0).floatValue();
+			inst.nametagBackgroundAlpha = nametags.getDouble("nametag_background_alpha", 0.5).floatValue();
+			inst.nametagScale = nametags.getDouble("nametag_scale", 1.0).floatValue();
+			inst.nametagYOffset = nametags.getDouble("nametag_y_offset", 0.75).floatValue();
+			inst.dmclootCompatRarity = nametags.getBoolean("dmcloot_compat_rarity", true);
+			inst.customRarities = nametags.getList("custom_rarities", new ArrayList<>());
+			inst.whiteRarities = nametags.getBoolean("white_rarities", false);
+			return inst;
 		}
 		else{
-			Configuration inst = new Configuration();
 			inst.save();
 			return inst;
 		}
-		return new Configuration();
 	}
 
 	public void save(){
-		File configFile = FabricLoader.getInstance().getConfigDir().resolve(LootBeams.MODID + ".json").toFile();
-		try (JsonWriter writer = new JsonWriter(new FileWriter(configFile))) {
-			GSON.toJson(this, Configuration.class, writer);
+		File configFile = FabricLoader.getInstance().getConfigDir().resolve(LootBeams.MODID + "-client.toml").toFile();
+		HashMap<String, Object> nametags = new HashMap<>();
+		nametags.put("borders", this.borders);
+		nametags.put("render_nametags", this.renderNametags);
+		nametags.put("render_nametags_onlook", this.renderNametagsOnlook);
+		nametags.put("render_stackcount", this.renderStackcount);
+		nametags.put("nametag_look_sensitivity", this.nametagLookSensitivity);
+		nametags.put("nametag_text_alpha", this.nametagTextAlpha);
+		nametags.put("nametag_background_alpha", this.nametagBackgroundAlpha);
+		nametags.put("nametag_scale", this.nametagScale);
+		nametags.put("nametag_y_offset", this.nametagYOffset);
+		nametags.put("dmcloot_compat_rarity", this.dmclootCompatRarity);
+		nametags.put("custom_rarities", this.customRarities);
+		nametags.put("white_rarities", this.whiteRarities);
+
+		HashMap<String, Object> items = new HashMap<>();
+		items.put("all_items", this.allItems);
+		items.put("only_rare", this.onlyRare);
+		items.put("only_equipment", this.onlyEquipment);
+		items.put("whitelist", this.whitelist);
+		items.put("blacklist", this.blacklist);
+
+		HashMap<String, Object> main = new HashMap<>();
+		main.put("render_name_color", this.renderNameColor);
+		main.put("render_rarity_color", this.renderRarityColor);
+		main.put("beam_radius", this.beamRadius);
+		main.put("beam_height", this.beamHeight);
+		main.put("beam_y_offset", this.beamYOffset);
+		main.put("beam_alpha", this.beamAlpha);
+		main.put("render_distance", this.renderDistance);
+		main.put("color_overrides", this.colorOverrides);
+		main.put("Items", items);
+		main.put("Nametags", nametags);
+
+		HashMap<String, Object> root = new HashMap<>();
+		root.put("Loot Beams", main);
+
+		var tomlWriter = new TomlWriter.Builder().indentValuesBy(4).indentTablesBy(4).padArrayDelimitersBy(1).build();
+		try {
+			tomlWriter.write(root, configFile);
 		} catch (IOException e) {
 			LootBeams.LOGGER.warn("cannot save config file", e);
 		}
